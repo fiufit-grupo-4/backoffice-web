c42b483fe33db0dd7107574cc6c555f538e2ab81
Dante Reinaudo <dantereinaudo@hotmail.com>
2023-06-21 20:02:48 -0300

    Adding users metrics

--- src/controllers/metrics/UserMetrics.js
+++ src/controllers/metrics/UserMetrics.js
@@ -1,15 +1,74 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Form,Table, Container,Row, Col, Spinner } from 'react-bootstrap';
+import { Card, Spinner } from 'react-bootstrap';
 import Sidebar from '../utils/SideBar';
-import { Link } from 'react-router-dom';
-import { API_GATEWAY, TOKEN, ADMIN, ATHLETE, TRAINER } from '../../utils/constants';
+
+import { API_GATEWAY, TOKEN } from '../../utils/constants';
+import { BarChart, Bar,LineChart, Line, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
 
 export default function UserMetrics() {
-    const [metrics, setMetrics] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
 
+    const [usersAuth, setUsersAuth] = useState({});
+    const [blockedUsers, setBlockedUsers] = useState({});
+    const [usersByLocation, setUsersByLocation] = useState({});
+
+    function generateUsersAuth(data) {
+      const transformedData = Object.entries(data).map(([endpoint, amount]) => ({
+        endpoint,
+        amount,
+      }));
+      setUsersAuth(transformedData)
+    }
+
+    function generateUsersByLoaction(data) {
+      const transformedData = Object.entries(data).map(([ value,amount]) => ({
+        country: value  === '' ?  'Unknown' : value,
+        amount 
+      }));
+      setUsersByLocation(transformedData)
+    }
+
+    function generateBlockedUsers(data) {
+      const transformedData = Object.entries(data).map(([date, amount]) => ({
+        date,
+        amount,
+      }));
+     
+      setBlockedUsers(transformedData)
+    }
+
+
+    async function getUsersHistory(url,accessToken,setResponse) {
+      let response =  await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + accessToken,    
+        }
+      })
+      if (!response.ok) {
+        setError(true)
+        if(response.status == 401) setErrorMessage("Unhautorized, not valid access token")
+        else setErrorMessage("Failed to connect with server")
+      } else {
+        let data = await response.json()
+        setResponse(data);  
+      }
+      
+    }
+
+    useEffect(() => {
+      const url = API_GATEWAY + 'history/'
+      const accessToken = localStorage.getItem(TOKEN)
+      setLoading(true)
+      getUsersHistory(url + 'users_auth',accessToken,generateUsersAuth);
+      getUsersHistory(url + 'blocked_users',accessToken,generateBlockedUsers);
+      getUsersHistory(url + 'users_by_location',accessToken,generateUsersByLoaction);
+      setLoading(false)  
+    }, []) 
 
     return (
         <div>
@@ -22,13 +81,53 @@ export default function UserMetrics() {
                 </div>
                
               : <div>
+                   <Card style = {{width:"90%",margin:"auto",padding:10}}>
+                      
+                      <Card.Body>
+                        <Card.Title style = {{marginBottom:20}} >Users Auth Information</Card.Title>
+                        <ResponsiveContainer width="100%" height={300}>
+                          <BarChart data={usersAuth} >
+                          <CartesianGrid stroke="#ccc"  />
+                            <XAxis dataKey="endpoint" tick={{ fill: 'white' }}   />
+                            <YAxis tick={{ fill: 'white' }} />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="amount" fill="#00bc8c"  />
+                          </BarChart>
+                        </ResponsiveContainer>
+                        <Card.Title style = {{marginBottom:20}} >Users By Location</Card.Title>
+                        <ResponsiveContainer width="100%" height={300}>
+                          <BarChart data={usersByLocation} >
+                            <CartesianGrid stroke="#ccc"  />
+                              <XAxis dataKey="country" tick={{ fill: 'white' }}   />
+                              <YAxis tick={{ fill: 'white' }} />
+                              <Tooltip />
+                              <Legend />
+                              <Bar dataKey="amount" fill="#fd7e14"  />
+                            </BarChart>
+                        </ResponsiveContainer>
+                        <Card.Title style = {{marginBottom:20}} >Blocked Users</Card.Title>
+                        <ResponsiveContainer width="100%" height={300}>
+                          <LineChart data={blockedUsers}>
+                          <CartesianGrid stroke="#ccc"  />
+                            <XAxis dataKey="date" tick={{ fill: 'white' }}   />
+                            <YAxis tick={{ fill: 'white' }} />
+                            <Tooltip />
+                            <Legend />
+                            <Line dataKey="amount" fill="#6f42c1" />
+                          
+                          </LineChart>
+                        </ResponsiveContainer>
 
-                {error && (
-                  <div className="d-flex justify-content-center align-items-center" style={{ marginTop: 10 }}>
-                    <p style = {{fontSize:18,color : "crimson",padding:5}}> {errorMessage} </p>
-                  </div>  
-                )}
-              </div>
+                        
+                      </Card.Body>
+                    </Card>
+                  {error && (
+                    <div className="d-flex justify-content-center align-items-center" style={{ marginTop: 10 }}>
+                      <p style = {{fontSize:18,color : "crimson",padding:5}}> {errorMessage} </p>
+                    </div>  
+                  )}
+                </div>
           }
         </div>
     )
